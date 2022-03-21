95c85babd0018b8d7cf6c9d1e9f19630ee35592c
GuillaumeCasbas <guillaume.casbas@gmail.com>
2022-03-21 18:09:24 +0000

    feat(router): add react-router to the project
    closes #20

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+  const text = screen.getByText(/It's board game time !/i);
+  expect(text).toBeInTheDocument();
 });
